2a700e96fcb6f0113f53c082233ac5aed2233692
margri764 <62308899+margri764@users.noreply.github.com>
2024-04-19 23:54:14 -0300

    pwa

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,9 +4,13 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimations(), provideAnimationsAsync()]
+  providers: [provideRouter(routes), provideAnimations(), provideAnimationsAsync(), provideServiceWorker('ngsw-worker.js', {
+        enabled: !isDevMode(),
+        registrationStrategy: 'registerWhenStable:30000'
+    })]
 };
 
 
